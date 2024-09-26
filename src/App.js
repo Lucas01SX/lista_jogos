c7aefab2c77f7223180d7d00503cfea95c8c0b1f
Lucas dos Santos Santana <lukassantana351@gmail.com>
2024-09-26 17:14:08 -0300

    Commit inicial do projeto

---
+++ src/App.js
@@ -0,0 +1,10 @@
+
+export default function App() {
+  return (
+    <>
+      <header>Aqui é o header</header>
+      <main> Aqui é o main</main>
+      <footer>Aqui é o footer</footer>
+    </>
+  );
+}
